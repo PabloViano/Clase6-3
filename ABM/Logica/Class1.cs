cbd0bf33bfa6032ee796bc58ef8345ffa9c80364
DAR-LAB-MA\UCSE <a>
2022-05-13 21:17:10 -0300

    Falta pasar info en los eventos

--- ABM/Logica/Class1.cs
+++ ABM/Logica/Class1.cs
@@ -1,18 +1,14 @@
 ﻿using Entidades;
+using ArgumentosEventos;
+
+using Persistencia;
 
 namespace Logica
 {
-    public class ComputadoraEventArgs : EventArgs
-    {
-        public string Procesador { get; set; }
-        public string PlacadeVideo { get; set; }
-        public decimal TamañoMemoriaRAM { get; set; }
-        public decimal TamañoDiscoDuro { get; set; }
-    }
     public class Class1
     {
-        private static Class1 instance = null;
-        public List<Computadora> Computadoras = new List<Computadora>();
+        private static Class1? instance = null;
+        List<Computadora> Computadoras = Persistencia.principal.Instance.LeerComputadoras();
         private Class1() { }
         public static Class1 Instance
         {
@@ -21,21 +17,36 @@ namespace Logica
                 if (instance == null) { instance = new Class1(); } return instance;
             }
         }
+        public EventHandler<ArgumentosEventos.Class1.AccionComputadoraEventArgs> AccionCompletada;
+        
         public void Alta(string proce, string placa, decimal ram, decimal disco)
         {
             Computadora computadora = new Computadora(Computadoras.Count + 1,proce, placa, ram, disco);
             Computadoras.Add(computadora);
+            GuardadoEventHandler(this, new ArgumentosEventos.Class1.AccionComputadoraEventArgs() { Accion = "Agregada", computadoras = this.Computadoras });
         }
-        public void Baja(string proce, string placa, decimal ram, decimal disco)
+        public void Baja(int id)
         {
-            Computadoras.Remove(Computadoras.Find(x => x.PlacadeVideo == placa && x.Procesador == proce && x.TamañoDiscoDuro == disco && x.TamañoMemoriaRAM == ram));
+            Computadoras.Remove(Computadoras.Find(x => x.ID == id));
+            GuardadoEventHandler(this, new ArgumentosEventos.Class1.AccionComputadoraEventArgs() { Accion = "Eliminada", computadoras = this.Computadoras });
         }
-        public void Modificacion (string proce, string placa, decimal ram, decimal disco)
+        public void Modificacion (int id,string proce, string placa, decimal ram, decimal disco)
         {
-            Computadoras.Find(x => x == computadora).Procesador = proce;
-            Computadoras.Find(x => x == computadora).PlacadeVideo = placa;
-            Computadoras.Find(x => x == computadora).TamañoMemoriaRAM = ram;
-            Computadoras.Find(x => x == computadora).TamañoDiscoDuro = disco;
+            foreach (var item in Computadoras)
+            {
+                if (item.ID == id)
+                {
+                    item.TamañoDiscoDuro = disco;
+                    item.Procesador = proce;
+                    item.TamañoMemoriaRAM = ram;
+                    item.PlacadeVideo = placa;
+                }
+            }
+            GuardadoEventHandler(this, new ArgumentosEventos.Class1.AccionComputadoraEventArgs() { Accion = "Modificada", computadoras = this.Computadoras });
+        }
+        public static void GuardadoEventHandler(object? sender, ArgumentosEventos.Class1.AccionComputadoraEventArgs e)
+        {
+            
         }
     }
 }
--- ABM/Presentacion/Program.cs
+++ ABM/Presentacion/Program.cs
@@ -1,15 +1,61 @@
 ﻿using Logica;
 
-Console.WriteLine("Que desea hacer (1 = cargar nueva computadora, 2 = salir del programa): ");
+Logica.Class1 logica = Logica.Class1.Instance;
+logica.AccionCompletada += AccionEventHandler;
+Console.WriteLine("Que desea hacer (1 = Continuar, 2 = salir del programa): ");
 int valor = int.Parse(Console.ReadLine());
 while (valor == 1)
 {
-    Console.WriteLine("Procesador (INTEL o AMD): ");
-    string proce = Console.ReadLine();
-    Console.WriteLine("Placa de video (NVDIA o AMD): ");
-    string placa = Console.ReadLine();
-    Console.WriteLine("Tamaño memoria RAM: ");
-    decimal tamañoRAM = decimal.Parse(Console.ReadLine());
-    Console.WriteLine("Tamaño disco duro: ");
-    decimal tamañoDiscoDuro = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Desea 3 = Dar de alta, 4 = Dar de baja, 5 = Modificar");
+    int accion = int.Parse(Console.ReadLine());
+    switch (accion)
+    {
+        case 3:
+            {
+                Console.WriteLine("Procesador (INTEL o AMD): ");
+                string proce = Console.ReadLine();
+                Console.WriteLine("Placa de video (NVDIA o AMD): ");
+                string placa = Console.ReadLine();
+                Console.WriteLine("Tamaño memoria RAM: ");
+                decimal tamañoRAM = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Tamaño disco duro: ");
+                decimal tamañoDiscoDuro = decimal.Parse(Console.ReadLine());
+                logica.Alta(proce, placa, tamañoRAM, tamañoDiscoDuro);
+            }
+            break;
+        case 4:
+            {
+                Console.WriteLine("Ingrese la ID de la computadora a eliminar; ");
+                int id = int.Parse(Console.ReadLine());
+                logica.Baja(id);
+            }
+            break;
+        case 5:
+            {
+                Console.WriteLine("Ingrese la ID de la computadora a eliminar; ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Procesador (INTEL o AMD): ");
+                string proce = Console.ReadLine();
+                Console.WriteLine("Placa de video (NVDIA o AMD): ");
+                string placa = Console.ReadLine();
+                Console.WriteLine("Tamaño memoria RAM: ");
+                decimal tamañoRAM = decimal.Parse(Console.ReadLine());
+                Console.WriteLine("Tamaño disco duro: ");
+                decimal tamañoDiscoDuro = decimal.Parse(Console.ReadLine());
+                logica.Modificacion(id,proce, placa, tamañoRAM, tamañoDiscoDuro);
+            }
+            break;
+        default:
+            {
+                Console.WriteLine("Numero incorrecto");
+            }
+            break;
+    }
+    Console.WriteLine("Que desea hacer (1 = Continuar, 2 = salir del programa): ");
+    valor = int.Parse(Console.ReadLine());
+
+}
+static void AccionEventHandler(object? sender, ArgumentosEventos.Class1.AccionComputadoraEventArgs e)
+{
+    Console.WriteLine($"La computadora fue {e.Accion} con exito!");
 }
