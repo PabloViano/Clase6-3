843c02be8da5febf4c55236888da2cf89ccbb5de
DAR-LAB-MA\UCSE <a>
2022-05-13 21:17:30 -0300

    Falta pasar info

---
+++ ABM/ArgumentosEventos/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Entidades;
+
+namespace ArgumentosEventos
+{
+    public class Class1
+    {
+        public class AccionComputadoraEventArgs : EventArgs
+        {
+            public string Accion { get; set; }
+            public List<Computadora> computadoras{ get; set; }
+        }
+        public class GuardadoCompletadoEventArgs : EventArgs
+        {
+            public bool Completo { get; set; }
+        }
+    }
+}
---
+++ ABM/Persistencia/principal.cs
@@ -0,0 +1,63 @@
+﻿using Entidades;
+using ArgumentosEventos;
+using Newtonsoft.Json;
+
+namespace Persistencia
+{
+    public class principal
+    {
+        private static principal? instance = null;
+        private principal() { }
+        public EventHandler<ArgumentosEventos.Class1.GuardadoCompletadoEventArgs> GuardadoCompleto;
+        public static principal Instance
+        {
+            get
+            {
+                if (instance == null) { instance = new principal(); }
+                return instance;
+            }
+        }
+
+        const string path = @"C:\Users\UCSE\OneDrive - Universidad Católica de Santiago del Estero\Documentos\Prog\Delegados_Eventos\Clase6-3\ABM\Computadoras.txt";
+        List<Computadora> computadoras = new List<Computadora>();
+        public List<Computadora> LeerComputadoras()
+        {
+            if (File.Exists(path))
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string computadora = reader.ReadToEnd();
+                    if (computadora != "")
+                    {
+                        computadoras = JsonConvert.DeserializeObject<List<Computadora>>(computadora);
+                    }
+                }
+            }
+            return computadoras;
+        }
+        public void GuardarComputadoras(List<Computadora> Computadoras)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+                using (StreamWriter writer = new StreamWriter(path, false))
+                {
+                    string ComputadorasjSON = JsonConvert.SerializeObject(Computadoras);
+                    writer.Write(ComputadorasjSON);
+                    if (GuardadoCompleto != null)
+                    {
+                        this.GuardadoCompleto(this, new ArgumentosEventos.Class1.GuardadoCompletadoEventArgs() { Completo = true });
+                    }
+                }
+            }
+            else
+            {
+                using (StreamWriter writer = new StreamWriter(path, false))
+                {
+                    string ComputadorasjSON = JsonConvert.SerializeObject(Computadoras);
+                    writer.Write(ComputadorasjSON);
+                }
+            }
+        }
+    }
+}
